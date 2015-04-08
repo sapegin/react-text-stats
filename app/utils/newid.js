fede4861619aee05ad289be67d4b876b2d84a77c
Artem Sapegin <artem@sapegin.ru>
2015-04-08 15:53:12 +0300

    Update 2015-04-08T12:53:05.025Z

--- app/utils/newid.js
+++
@@ -1,8 +0,0 @@
-// Author: Artem Sapegin http://sapegin.me, 2015
-
-let lastId = 0;
-
-export default function(prefix='id') {
-	lastId++;
-	return `${prefix}${lastId}`;
-}
--- app/utils/react-extras.js
+++
@@ -1,7 +0,0 @@
-import _ from 'lodash';
-
-export let RestAttrsMixin = {
-	restAttrs() {
-		return _.omit(this.props, _.keys(this.constructor.propTypes));
-	}
-};
