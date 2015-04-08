62e630a91f7bd6f8aca49cf7c884d4fbf476ccb6
Artem Sapegin <artem@sapegin.ru>
2015-04-08 16:15:56 +0300

    Fix import.

--- app/app.js
+++ app/app.js
@@ -1,5 +1,5 @@
 'use strict';
 
-import Application from './components/application';
+import Application from './components/Application';
 
 React.render(React.createElement(Application), document.getElementById('app'));
